341cee05922bd7c9fbb2099a609114c1c33afa1a
Dominick Simone <domsimone123@gmail.com>
2021-08-28 19:13:55 -0400

    Finished everything except README

--- index.js
+++ index.js
@@ -28,7 +28,28 @@ const addRoleQuestions = [{
     message: "What is your roles department ID?"
 }
 ]
-function viewDepartments() { 
+const addEmployeeQuestions = [{
+    name: "firstName",
+    type: "input",
+    message: "What the new employee's first name?",
+},
+{
+    name: "lastName",
+    type: "input",
+    message: "What is the employee's last name?"
+},
+{
+    name: "managerId",
+    type: "input",
+    message: "Who is this employees manager?(Input the managers id)"
+},
+{
+    name: "roleId",
+    type: "input",
+    message: "What is your employee's role? (Input the roles id)"
+}
+]
+function viewDepartments() {
     connection.query("SELECT * FROM departments", (err, results) => {
         if (err) {
             console.log(err)
@@ -60,44 +81,67 @@ async function addDepartment() {
         .prompt(addDepartmentQuestions)
         .then((response) => {
             connection.query(`INSERT INTO departments(name) VALUES (?)`, response.department, (err, results) => {
-            if (err) {
-                console.log(err)
-            }            
-        });
-    })
-    mainMenu()
+                if (err) {
+                    console.log(err)
+                }
+                connection.query("SELECT * FROM departments", (err, results) => {
+                    console.table(results)
+                    mainMenu()
+                })
+            });
+        })
+    
 }
-async function addRole() {
+function addRole() {
     connection.query("SELECT * FROM departments", (err, results) => {
         console.table(results)
         if (err) {
             console.log(err)
         }
+        inquirer
+            .prompt(addRoleQuestions)
+            .then((response) => {
+                connection.query(`INSERT INTO role(title,salary,department_id) VALUES (?,?,?)`, [response.title, response.salary, response.departmentId], (err, results) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    connection.query("SELECT * FROM role", (err, results) => {
+                        console.table(results)
+                        mainMenu()
+                    })
+                });
+            })
     })
-    await inquirer
-        .prompt(addRoleQuestions)
-        .then((response) => {
-            connection.query(`INSERT INTO role(title,salary,department_id) VALUES (?,?,?)`, [response.title, response.salary, response.departmentId], (err, results) => {
-                if (err) {
-                    console.log(err)
-                }
-                connection.query("SELECT * FROM role", (err, results) => {
-                    console.table(results)
-            })            
-        });
+}
+function addEmployee() {
+    connection.query("SELECT * FROM role", (err, results) => {
+        console.table(results)
+        connection.query("SELECT * FROM employee", (err, results) => {
+            console.table(results)
+            inquirer
+                .prompt(addEmployeeQuestions)
+                .then((response) => {
+                    connection.query("INSERT INTO employee(first_name,last_name,manager_id,role_id) VALUES (?,?,?,?)", [response.firstName, response.lastName, response.managerId, response.roleId], (err, results) => {
+                        connection.query("SELECT * FROM employee", (err, results) => {
+                            console.table(results)
+                            mainMenu()
+                        })
+                    })
+                })
+        })
     })
-    mainMenu()
+
 }
 function updateRole() {
     connection.query("SELECT * FROM role", (err, results) => {
         console.table(results)
         const rolesChoice = results.map(obj => {
-            return {name: obj.title, value: obj.id};
+            return { name: obj.title, value: obj.id };
         })
         connection.query("SELECT * FROM employee", (err, results) => {
             console.table(results)
             const employeeChoice = results.map(obj => {
-                return {name: obj.first_name, value: obj.id}
+                return { name: obj.first_name, value: obj.id }
             })
             const newQuestion = [{
                 name: "employees",
@@ -112,39 +156,39 @@ function updateRole() {
                 choices: rolesChoice
             }]
             inquirer
-            .prompt(newQuestion)
-            .then((response) => {
-                connection.query("UPDATE employee SET role_id = ? WHERE id = ?", [response.roles, response.employees], (err, results) => {
+                .prompt(newQuestion)
+                .then((response) => {
+                    connection.query("UPDATE employee SET role_id = ? WHERE id = ?", [response.roles, response.employees], (err, results) => {
                         connection.query("SELECT * FROM employee", (err, results) => {
                             console.table(results)
                             mainMenu()
                         })
                     })
-            })
+                })
         })
     })
 }
 function mainMenu() {
-inquirer 
-    .prompt(menuQuestion)
-    .then((response) => {
-        if (response.menu === "View all departments") {
-            viewDepartments()
-        } else if (response.menu === "View all roles") {
-            viewRoles()
-        } else if (response.menu === "View all employees") {
-            viewEmployees()
-        } else if (response.menu === "Add a department") {
-            addDepartment()
-        } else if (response.menu === "Update a role") {
-            updateRole()
-        } else if (response.menu === "Add an Employee") {
-            addEmployee()
-        } else if (response.menu === "I'm Done") {
-            return;
-        } else if (response.menu === "Add a role") {
-            addRole()
-        }
-    })
+    inquirer
+        .prompt(menuQuestion)
+        .then((response) => {
+            if (response.menu === "View all departments") {
+                viewDepartments()
+            } else if (response.menu === "View all roles") {
+                viewRoles()
+            } else if (response.menu === "View all employees") {
+                viewEmployees()
+            } else if (response.menu === "Add a department") {
+                addDepartment()
+            } else if (response.menu === "Update a role") {
+                updateRole()
+            } else if (response.menu === "Add an employee") {
+                addEmployee()
+            } else if (response.menu === "I'm Done") {
+                return;
+            } else if (response.menu === "Add a role") {
+                addRole()
+            }
+        })
 }
 mainMenu()
